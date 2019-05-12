89138c50aebd3a44ff5f580ee7aff416fe82f31d
utkarsh1367 <utkarsh1367@gmail.com>
2019-05-12 21:20:43 +1000

    update avengers

---
+++ Ansible/roles/openstack-couchdb-master/tasks/add_cluster_node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export masternode=$1
+export node=$2
+export user=admin
+export pass=admin
+curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
+      --header "Content-Type: application/json" \
+      --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \
+        \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": \"5984\", \
+        \"remote_node\": \"${node}\", \
+        \"remote_current_user\":\"${user}\", \"remote_current_password\":\"${pass}\"}"
+
+curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
+      --header "Content-Type: application/json" \
+      --data "{\"action\": \"add_node\", \"host\":\"${node}\", \
+        \"port\": \"5984\", \"username\": \"${user}\", \"password\":\"${pass}\"}"
