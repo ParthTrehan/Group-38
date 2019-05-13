17274e0ec26c0b482993ac398fde6a68f514b5e3
utkarsh1367 <utkarsh1367@gmail.com>
2019-05-13 16:19:29 +1000

    update

--- Ansible/roles/openstack-couchdb-master/tasks/add_cluster_node.sh
+++ Ansible/roles/openstack-couchdb-master/tasks/add_cluster_node.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 export masternode=$1
 export node=$2
-export user=admin
-export pass=admin
-curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
+curl -XPOST "http://admin:admin@${masternode}:5984/_cluster_setup" \
       --header "Content-Type: application/json" \
       --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \
-        \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": \"5984\", \
+        \"username\": \"admin\", \"password\":\"admin\", \"port\": \"5984\", \
         \"remote_node\": \"${node}\", \
-        \"remote_current_user\":\"${user}\", \"remote_current_password\":\"${pass}\"}"
+        \"remote_current_user\":\"admin\", \"remote_current_password\":\"admin\"}"
 
-curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
+curl -XPOST "http://admin:admin@${masternode}:5984/_cluster_setup" \
       --header "Content-Type: application/json" \
       --data "{\"action\": \"add_node\", \"host\":\"${node}\", \
-        \"port\": \"5984\", \"username\": \"${user}\", \"password\":\"${pass}\"}"
+        \"port\": \"5984\", \"username\": \"admin\", \"password\":\"admin\"}"
