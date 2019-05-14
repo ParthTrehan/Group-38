219a71188f614d7767687ec2fb579dec068fc8cf
rkirpekar <rkirpekar@student.unimelb.edu.au>
2019-05-14 22:52:17 +1000

    Added script to send views to and run mapreduce periodically

--- Data/get_all_tweets.py
+++ Data/get_all_tweets.py
@@ -1,16 +1,19 @@
 import couchdb
 import json
-data = json.load(open('all_tweets.json'))
-config = json.load(open('/home/ubuntu/deploy/Twitter_Crawler/db_config.json'))
-tweets = data["rows"]
+
+config = json.load(open('../Twitter_Crawler/db_config.json'))
 server_address = config["db_url"]
-tweets_db_name = config["tweets_db_name"]
-users_db_name = config["users_db_name"]
+tweets_db_name = "mcbc"
 couch = couchdb.Server(server_address)
+
 try:
     tweets_db = couch[tweets_db_name]
 except:
     tweets_db = couch.create(tweets_db_name)
-
-for tweet in tweets:
-    tweets_db.save(tweet["doc"])
+with open("all_tweets.json", encoding='utf8') as f:
+        #print(line)
+        tweets = json.load(f)
+        tweets = tweets["rows"]
+        for tweet in tweets:
+            single = tweet["doc"]
+            
