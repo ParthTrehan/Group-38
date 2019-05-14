9badc3d287fe4c147a4d8db0da611f7699f8266d
utkarsh1367 <utkarsh1367@gmail.com>
2019-05-15 03:17:04 +1000

    50 Hrs

--- Ansible/run-nectar.sh
+++ Ansible/run-nectar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm hosts
-echo -e "[webserver]\n[harvesters]\n[all]" > hosts
-chmod 777 hosts
+#rm hosts
+# echo -e "[webserver]\n[harvesters]\n[all]" > hosts
+# chmod 777 hosts
 . ./openrc.sh;
-ansible-playbook -i hosts --ask-become-pass nectar.yaml
-sleep 180s
+# ansible-playbook -i hosts --ask-become-pass nectar.yaml
+# sleep 180s
 ansible-playbook -i hosts --ask-become-pass instance_init.yaml
