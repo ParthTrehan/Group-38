e0cd1e99f07b37321001715c0dfddd1c1421a9c3
utkarsh1367 <utkarsh1367@gmail.com>
2019-05-15 02:05:26 +1000

    ENDGAME

--- Data/send_updated_result.sh
+++ Data/send_updated_result.sh
@@ -2,6 +2,6 @@
 curl -X PUT http://admin:admin@localhost:5984/tweets/_design/tweets-reduce -d @views.json
 while true; do
     curl http://admin:admin@127.0.0.1:5984/tweets/_design/tweets-reduce/_view/all-tweets?group_level=3 > new_result.json;
-    python send_new_result.py 
+    python3 send_new_result.py 
     sleep 60;
 done
